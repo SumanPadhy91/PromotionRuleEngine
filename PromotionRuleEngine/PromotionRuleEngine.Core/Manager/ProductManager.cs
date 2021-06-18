a31c45322dffba906c1f3bbd29f496c41abc14ed
Suman <suman@cloudthing.com>
2021-06-18 13:08:32 +0530

    Product Manager

---
+++ PromotionRuleEngine/PromotionRuleEngine.Core/Manager/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using PromotionRuleEngine.Core.Manager.Interfaces;
+using PromotionRuleEngine.Core.Models;
+using System.Collections.Generic;
+
+namespace PromotionRuleEngine.Core.Manager
+{
+    public class ProductManager : IProductManager
+    {
+        private List<Product> products;
+
+        public ProductManager()
+        {
+            this.products = new List<Product>();
+        }
+
+        public void AddProduct(string id, decimal price)
+        {
+            this.products.Add(new Product(id.ToUpper(), price));
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return this.products;
+        }
+    }
+}
--- PromotionRuleEngine/RuleEngine.Tests/RuleEngineTest.cs
+++ PromotionRuleEngine/RuleEngine.Tests/RuleEngineTest.cs
@@ -1,14 +1,35 @@
-using System;
+using Microsoft.Extensions.DependencyInjection;
+using PromotionRuleEngine.Core.Manager;
+using PromotionRuleEngine.Core.Manager.Interfaces;
+using System.Linq;
 using Xunit;
 
 namespace RuleEngine.Tests
 {
     public class RuleEngineTest
     {
-        [Fact]
-        public void Test1()
-        {
+        IProductManager productManager;
 
+        public RuleEngineTest()
+        {
+              var serviceProvider = new ServiceCollection()
+                .AddScoped<IProductManager, ProductManager>()
+                .BuildServiceProvider();
+
+            this.productManager = serviceProvider.GetRequiredService<IProductManager>();
+
+            productManager.AddProduct("A", 50);
+            productManager.AddProduct("b", 30);
+            productManager.AddProduct("C", 20);
+            productManager.AddProduct("D", 15);
+        }
+
+        [Fact]
+        public void ProductsTest()
+        {
+            var products = productManager.GetAllProducts();
+
+            Assert.Equal(4, products.Count());
         }
     }
 }
