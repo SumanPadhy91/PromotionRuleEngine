2c2cab7bc390ecc29d5d514225e91b6aed293a52
Suman <suman@cloudthing.com>
2021-06-18 15:14:53 +0530

    Cart Manager

---
+++ PromotionRuleEngine/PromotionRuleEngine.Core/Manager/CartManager.cs
@@ -0,0 +1,56 @@
+﻿using PromotionRuleEngine.Core.Manager.Interfaces;
+using PromotionRuleEngine.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionRuleEngine.Core.Manager
+{
+    public class CartManager : ICartManager
+    {
+        private Dictionary<Product, int> cartItems;
+
+        public CartManager()
+        {
+            this.cartItems = new Dictionary<Product, int>();
+        }
+
+        public void AddCartItems(Product product, int quantity)
+        {
+            if (this.cartItems.ContainsKey(product))
+            {
+                this.cartItems[product] += quantity;
+            }
+            else
+            {
+                this.cartItems.Add(product, quantity);
+            }
+        }
+
+        public Dictionary<Product, int> GetCartItems()
+        {
+            return this.cartItems;
+        }
+
+        public int GetTotalCartItems()
+        {
+            var count = 0;
+            foreach (var item in this.cartItems)
+            {
+                count += item.Value;
+            }
+            return count;
+        }
+
+        public ICartManager Clone()
+        {
+            var cart = new CartManager();
+            foreach (var item in this.cartItems)
+            {
+                cart.cartItems.Add(item.Key, item.Value);
+            }
+            return cart;
+        }
+    }
+}
+
--- PromotionRuleEngine/RuleEngine.Tests/RuleEngineTest.cs
+++ PromotionRuleEngine/RuleEngine.Tests/RuleEngineTest.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using PromotionRuleEngine.Core.Manager;
 using PromotionRuleEngine.Core.Manager.Interfaces;
 using PromotionRuleEngine.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -12,16 +13,19 @@ namespace RuleEngine.Tests
     {
         IProductManager productManager;
         IPromotionManager promotionManager;
+        ICartManager cartManager;
 
         public RuleEngineTest()
         {
             var serviceProvider = new ServiceCollection()
                 .AddScoped<IProductManager, ProductManager>()
                 .AddScoped<IPromotionManager, PromotionManager>()
+                .AddScoped<ICartManager, CartManager>()
                 .BuildServiceProvider();
 
             this.productManager = serviceProvider.GetRequiredService<IProductManager>();
             this.promotionManager = serviceProvider.GetRequiredService<IPromotionManager>();
+            this.cartManager = serviceProvider.GetRequiredService<ICartManager>();
 
             productManager.AddProduct("A", 50);
             productManager.AddProduct("b", 30);
@@ -50,6 +54,42 @@ namespace RuleEngine.Tests
             var products = productManager.GetAllProducts();
 
             Assert.Equal(4, products.Count());
+        }
+
+        [Fact]
+        public void ItemsinCartTest()
+        {
+            var products = productManager.GetAllProducts();
+
+            cartManager.AddCartItems(products.Where(c => c.Sku == "A").FirstOrDefault(), 3);
+            cartManager.AddCartItems(products.Where(c => c.Sku == "B").FirstOrDefault(), 5);
+            cartManager.AddCartItems(products.Where(c => c.Sku == "C").FirstOrDefault(), 1);
+            cartManager.AddCartItems(products.Where(c => c.Sku == "D").FirstOrDefault(), 1);
+
+            var cartItems = cartManager.GetCartItems();
+
+            Assert.Equal(4, cartItems.Count());
+            Assert.Equal(10, cartManager.GetTotalCartItems());
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 0, 100)]
+        [InlineData(5, 5, 1, 0, 370)]
+        [InlineData(3, 5, 1, 1, 280)]
+        public void VerifyCartValue(object valueA, object valueB, object valueC, object valueD, object expected)
+        {
+            var products = this.productManager.GetAllProducts();
+            cartManager.AddCartItems(products.Where(c => c.Sku == "A").FirstOrDefault(), Convert.ToInt32(valueA));
+            cartManager.AddCartItems(products.Where(c => c.Sku == "B").FirstOrDefault(), Convert.ToInt32(valueB));
+            cartManager.AddCartItems(products.Where(c => c.Sku == "C").FirstOrDefault(), Convert.ToInt32(valueC));
+            cartManager.AddCartItems(products.Where(c => c.Sku == "D").FirstOrDefault(), Convert.ToInt32(valueD));
+
+            var cart = cartManager.Clone();
+            var resp = promotionManager.CalculateDiscount(cart);
+
+            Assert.Equal(Convert.ToDecimal(expected), resp);
+            Assert.Equal(Convert.ToDecimal(expected), resp);
+            Assert.Equal(Convert.ToDecimal(expected), resp);
         }
     }
 }
